63b6e8abeb53fec6010de0108aeebc561da8fccb
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-07-10 13:58:44 +0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import Chat from "./components/chat/Chat"
+import Details from "./components/details/Details"
+import List from "./components/list/List"
+
+
 const App = () => {
   return (
-    <div className=''>App</div>
+    <div className='container'>
+
+    <List/>
+    <Chat/>
+    <Details/>
+
+    </div>
   )
 }
+
+
 
 export default App
---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./chat.css"
+
+export default function Chat() {
+  return (
+
+
+    <div>
+      chats
+    </div>
+
+
+)
+}
