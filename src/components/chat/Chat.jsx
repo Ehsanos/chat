71f83dd9ec30049fbdea64bad3c4329f3a22b981
Ehsano Gamor <ehsan.gamor90@gmail.com>
2024-07-15 12:46:24 +0300

    adding chat handel+imoji picker

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,12 +1,70 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react"
 
 export default function Chat() {
+
+
+const [EmojiView,setEmojiView]=useState(false)
+
+const [text,setText] = useState(" ")
+
+const handel=e=>{
+
+  console.log(e);
+}
+
+console.log(text);
+
   return (
 
 
     <div className='chat'>
-      chats
+
+<div className="top">
+<div className="user">
+  <img src="./avatar.png"/>
+  <div className="texts">
+    <span className="">jon Doe</span>
+    <p>lorem ipsum dolor sit amet, consectetur</p>
+  </div>
+</div>
+
+<div className="icons">
+<img src='./phone.png'/>
+<img src='./video.png'/>
+<img src='./info.png'/>
+</div>
+
+</div>
+
+
+<div className="center"></div>
+
+
+<div className="bottom">
+
+<div className="icons">
+<img src='./img.png'/>
+<img src='./camera.png'/>
+<img src='./mic.png'/>
+</div>
+<input type="text" placeholder='write message ......' onChange={(e)=>setText(e.target.value)}/>
+<div className="emoji">
+<img src='./emoji.png'  onClick={()=>setEmojiView((prive)=>!prive)}/>
+
+<EmojiPicker   open={EmojiView} onEmojiClick={handel}/>
+
+
+
+</div>
+<button className='sendBtn'>Send</button>
+
+</div>
+
+
+
+
     </div>
 
 
